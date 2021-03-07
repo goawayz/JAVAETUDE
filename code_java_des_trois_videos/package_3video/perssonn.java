52a5554c5419f48a9f5fa22e2cd119f1da397026
djamel <djamel22ks@gmail.com>
2021-03-07 20:44:29 +0100

    j'ai ajouter un commentair

--- code_java_des_trois_videos/package_3video/perssonn.java
+++ code_java_des_trois_videos/package_3video/perssonn.java
@@ -1,6 +1,6 @@
 package dz.abdo.program;
 
-public interface perssonn {//creat interface personn
+public interface perssonn {//creat  personn comme interface
 }
 class Etudiante implements perssonn {}//la classe implements from l'interface(par se que la line ou l'association discontinue dans la video deuxiem partie)
 class Enseignat implements perssonn {}//Enseingant implements from linterface personn
